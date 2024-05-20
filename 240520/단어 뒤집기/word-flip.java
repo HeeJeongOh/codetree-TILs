8ea6894a2011fe7616177247e7a299d7b69022b8
HEEJEONG OH <62127798+HeeJeongOh@users.noreply.github.com>
2024-05-20 14:55:16 +0900

    Title: 단어 뒤집기, Time: 499ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240520/단어 뒤집기/word-flip.java
+++ 240520/단어 뒤집기/word-flip.java
@@ -3,39 +3,45 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
+        String[] input = sc.nextLine().split("");
 
-        int n = input.length();
+        int n = input.length;
         boolean reverse = true;
 
         StringBuffer result = new StringBuffer();
         StringBuffer tmp = new StringBuffer();
 
         for(int i = 0; i < n; i++){
-            char letter = input.charAt(i);
-            // System.out.println(letter + " " + tmp);
-
-            if(letter == '<'){
+            String letter = input[i];
+            // System.out.print(letter + " " + tmp);
+            // System.out.println("  / result:" + result);
+            if(letter.equals(" ")){
+                result.append(tmp.reverse());
+                result.append(" ");
+                tmp = new StringBuffer();
+            }
+            else if(letter.equals("<")){
                 if(tmp.length() > 0) {
                     result.append(tmp.reverse()); 
-                    // System.out.println("중간점검: " + result);
                     tmp = new StringBuffer();
                 }
                 tmp.append(letter);
                 reverse = false;
             }
-            else if (letter == '>'){
+            else if (letter.equals(">")){
                 tmp.append(letter);
                 reverse = true;
 
                 result.append(tmp);
-                // System.out.println("중간점검: " + result);
                 tmp = new StringBuffer();
-            }     
+            }
             else{
                 tmp.append(letter);
             }       
         }   
+        if(tmp.length() > 0) {
+            result.append(tmp.reverse()); 
+        }
         System.out.println(result);            
     }    
 }
