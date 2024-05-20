e4b290e10be199037cda3356cf15f1deee5805e5
HEEJEONG OH <62127798+HeeJeongOh@users.noreply.github.com>
2024-05-20 15:11:29 +0900

    Title: 단어 뒤집기, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240520/단어 뒤집기/word-flip.java
+++ 240520/단어 뒤집기/word-flip.java
@@ -3,45 +3,41 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] input = sc.nextLine().split("");
-
-        int n = input.length;
-        boolean reverse = true;
+        String[] input = sc.nextLine().split(" ");
 
         StringBuffer result = new StringBuffer();
         StringBuffer tmp = new StringBuffer();
 
-        for(int i = 0; i < n; i++){
-            String letter = input[i];
-            // System.out.print(letter + " " + tmp);
-            // System.out.println("  / result:" + result);
-            if(letter.equals(" ")){
-                result.append(tmp.reverse());
-                result.append(" ");
-                tmp = new StringBuffer();
-            }
-            else if(letter.equals("<")){
-                if(tmp.length() > 0) {
-                    result.append(tmp.reverse()); 
+        for(String word : input){
+            // System.out.println(word);
+            int n = word.length();
+            boolean reverse = true;
+            tmp = new StringBuffer("");
+
+            for(int i = 0; i < n; i++){
+                char letter = word.charAt(i);
+                // System.out.println(letter + " " +tmp + " " + result);
+
+                if(letter == '<'){
+                    result.append(tmp.reverse());
+                    tmp = new StringBuffer("");
+
+                    tmp.append("<");
+                    reverse = false;
+                }
+                else if (letter == '>'){
+                    result.append(tmp);
+                    result.append(">");
+
                     tmp = new StringBuffer();
                 }
-                tmp.append(letter);
-                reverse = false;
+                else{
+                    tmp.append(letter);
+                }
             }
-            else if (letter.equals(">")){
-                tmp.append(letter);
-                reverse = true;
-
-                result.append(tmp);
-                tmp = new StringBuffer();
-            }
-            else{
-                tmp.append(letter);
-            }       
-        }   
-        if(tmp.length() > 0) {
-            result.append(tmp.reverse()); 
+            result.append(tmp + " ");
         }
-        System.out.println(result);            
+        System.out.println(result);
+        
     }    
 }
