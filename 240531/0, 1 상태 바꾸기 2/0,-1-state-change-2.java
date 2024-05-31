9865d4626c8329b0d5f2cf82f6ae9f6c1ad94186
HEEJEONG OH <62127798+HeeJeongOh@users.noreply.github.com>
2024-05-31 16:23:42 +0900

    Title: 0, 1 상태 바꾸기 2, Time: 150ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240531/0, 1 상태 바꾸기 2/0,-1-state-change-2.java
+++ 240531/0, 1 상태 바꾸기 2/0,-1-state-change-2.java
@@ -3,6 +3,12 @@ import java.util.*;
 
 */
 public class Main {
+    static int swap(int n){
+        if(n == 1) {
+            return 0;
+        }
+        return 1;
+    }
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
@@ -11,24 +17,37 @@ public class Main {
         for(int i = 1; i < n+1; i++){
             switches[i] = sc.nextInt();
         }
-        System.out.println(Arrays.toString(switches));
+        // System.out.println(Arrays.toString(switches));
 
         int m = sc.nextInt();
         for(int i = 0; i < m; i++){
             int type = sc.nextInt();
-            int switchNum = sc.nextInt() ;
-            System.out.println(type + " " + switchNum);
+            int snum = sc.nextInt() ;
+            // System.out.println(type + " " + snum);
 
             if(type == 1){
-                for(int pos = switchNum; pos <= n+1; i*= 2){
-                    if(switches[pos] == 1) {switches[pos] = 0;}
-                    else {switches[pos] = 1;}
+                for(int pos = snum; pos < n+1; pos *= 2){
+                    switches[pos] = swap(switches[pos]);
                 }
-                System.out.println(Arrays.toString(switches));
             }
-            else if (type == 2){
+            else {
+                switches[snum] = swap(switches[snum]);
+                int j = 1;
+                while(0 < snum-j && snum+j < n+1){
+                    if(switches[snum+j] != switches[snum-j]){
+                         break; 
+                    }
+                    switches[snum-j] = swap(switches[snum-j]);
+                    switches[snum+j] = swap(switches[snum+j]);
+                    j+= 1;
+                }
+            }
+            // System.out.println(Arrays.toString(switches));
+        }
+        // System.out.println(Arrays.toString(switches));
 
-            }
+        for(int i = 1; i < n+1; i++){
+            System.out.print(switches[i] + " ");
         }
     }
 }
