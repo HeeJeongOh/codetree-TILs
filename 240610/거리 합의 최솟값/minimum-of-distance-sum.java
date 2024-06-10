d8f8e7e22d5068ec1e635fbe082b3c29633df1b7
HEEJEONG OH <62127798+HeeJeongOh@users.noreply.github.com>
2024-06-10 15:15:10 +0900

    Title: 거리 합의 최솟값, Time: 367ms, Memory: 23MB, Status: Passed - CodeTree

--- 240610/거리 합의 최솟값/minimum-of-distance-sum.java
+++ 240610/거리 합의 최솟값/minimum-of-distance-sum.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 /*
 검색 : 평균값이 아닌 중앙값 찾기
+1 3 5 9 12 14 
 */
 
 public class Main {
@@ -10,12 +11,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
-        double[] xs = new double[n];
-        double[] ys = new double[n];
+        int[] xs = new int[n];
+        int[] ys = new int[n];
 
         for(int i = 0; i < n; i++){
-            double x = sc.nextInt();
-            double y = sc.nextInt();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
             
             xs[i] = x;
             ys[i] = y;
@@ -25,25 +26,25 @@ public class Main {
         Arrays.sort(ys);
 
         // 중앙값 계산
-        double xMedian = xs[n / 2];
-        double yMedian = ys[n / 2];
+        int xMedian = xs[n / 2];
+        int yMedian = ys[n / 2];
 
         // 요소가 짝수 개일 경우, 두 중앙값의 평균 계산
         if (n % 2 == 0) {
-            xMedian = (xs[n / 2 - 1] + xs[n / 2]) / 2;
-            yMedian = (ys[n / 2 - 1] + ys[n / 2]) / 2;
+            xMedian = (xs[n / 2 - 1] + xMedian) / 2;
+            yMedian = (ys[n / 2 - 1] + yMedian) / 2;
         }
 
         // System.out.println("X 좌표들의 중앙값: " + xMedian);
         // System.out.println("Y 좌표들의 중앙값: " + yMedian);
 
         // 거리 합 계산
-        double totalDistance = 0; 
+        long totalDistance = 0; 
 
         for(int i = 0; i < n; i++){
             totalDistance += Math.abs(xMedian - xs[i]) + Math.abs(yMedian - ys[i]);
         }
 
-        System.out.println((int)totalDistance);
+        System.out.println(totalDistance);
     }
 }
