a16ec27536800fb4b915a7e5047d0802cbdd7c24
HEEJEONG OH <62127798+HeeJeongOh@users.noreply.github.com>
2024-06-11 15:11:39 +0900

    Title: 좌표평면 위의 특정 구역, Time: 1006ms, Memory: 45MB, Status: Passed - CodeTree

--- 240611/좌표평면 위의 특정 구역/specific-zone-above-the-2d-coordinate.java
+++ 240611/좌표평면 위의 특정 구역/specific-zone-above-the-2d-coordinate.java
@@ -1,23 +1,17 @@
 import java.util.*;
 /*
-- 하나의 점을 빼서, 모든 점들을 포함하는 직사각형의 넓이를 최소로 하기
-
-1. 어떤 점을 뺄 것인가
-2. 넓이 = (최대x-최소x) * (최대y-최소y)
-3. 최대x,y가 같은 점이 아님
-4. 
-
+[시도2] 
+1. 최대최솟값이 포함된 점들만 제거해보기 - 최대 4개의 점
 */
 public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
+    public static void main(String[] args) {   
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        
+
         int[] xs = new int[n];
         int[] ys = new int[n];
-        
+        int[][] points = new int[n][2];
         for(int i = 0; i < n; i++){
             int x = sc.nextInt();
             int y = sc.nextInt();
@@ -25,34 +19,55 @@ public class Main {
             xs[i] = x;
             ys[i] = y;
 
-            // System.out.print("(" + x + " " + y + ") /");
+            points[i] = new int[]{x, y};
         }
-        // System.out.println();
 
-        // 모든 경우 다 넓이 구하기
-        long min_area = 1600000000;
+        Arrays.sort(xs);
+        Arrays.sort(ys);
+
+        // System.out.println(xs[0]+ " " + xs[n-1] + " " + ys[0] + " " + ys[n-1]);
+
+        Set<Integer> indexes = new HashSet<>();
         for(int i = 0; i < n; i++){
-            int minx = 40000, miny = 40000;
-            int maxx = 0, maxy = 0;
-
-            for(int j = 0; j < n; j++){
-                if(i == j) { continue; }
-                
-                if(xs[j] < minx){ minx = xs[j]; }
-                if (maxx < xs[j]){ maxx = xs[j]; }
-
-                if(ys[j] < miny){ miny = ys[j]; }
-                if (maxy < ys[j]){ maxy = ys[j]; }
+            // System.out.println(Arrays.toString(points[i]));
+            if(points[i][0] == xs[0]){
+                indexes.add(i);
             }
-
-            long tmp = (maxx-minx) * (maxy-miny);
-
-            // System.out.println(i + " (" + minx + " " + miny + ") * (" + maxx + " " + maxy + ") => " + tmp);
-
-            if(tmp < min_area){
-                min_area = tmp;
+            if(points[i][0] == xs[n-1]){
+                indexes.add(i);
+            }
+            if(points[i][1] == ys[0]){
+                indexes.add(i);
+            }
+            if(points[i][1] == ys[n-1]){
+                indexes.add(i);
             }
         }
-        System.out.println(min_area);
+        // System.out.println(indexes);
+
+        long area = 1600000000;
+        for(int idx : indexes){
+            int[] min = new int[]{40000, 40000};
+            int[] max = new int[]{0, 0};
+
+            for(int i = 0; i < n; i++){
+                if(i == idx) { continue; }
+
+                int x = points[i][0];
+                int y = points[i][1];
+
+                if( x < min[0]) { min[0] = x; }
+                if( y < min[1]) { min[1] = y; }
+
+                if( max[0] < x) { max[0] = x; }
+                if( max[1] < y) { max[1] = y; }
+            }
+
+            long tmp = (max[0]-min[0])*(max[1]-min[1]);
+            if(tmp < area){ area = tmp;}
+
+            // System.out.println(Arrays.toString(min) + " " + Arrays.toString(max) + " " + tmp);
+        }
+        System.out.println(area);
     }
 }
