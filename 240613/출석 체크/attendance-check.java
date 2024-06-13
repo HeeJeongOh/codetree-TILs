70eee73dc9f87c235874a051092c23d7e87c0e59
HEEJEONG OH <62127798+HeeJeongOh@users.noreply.github.com>
2024-06-13 17:12:01 +0900

    Title: 출석 체크, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240613/출석 체크/attendance-check.java
@@ -0,0 +1,66 @@
+import java.util.*;
+/*
+- 수업 시작시간 전에 활동 기록 있을 것
+- 수업 종료 후 
+*/
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int s = Integer.parseInt(sc.next().replace(":", ""));
+        int e = Integer.parseInt(sc.next().replace(":", ""));
+        int q = Integer.parseInt(sc.next().replace(":", ""));
+        int n = sc.nextInt();
+
+        // System.out.println(s + " " + e + " " + q);
+        
+        Map<String, ArrayList<String>> students = new TreeMap<>();
+        for(int i = 0; i < n; i++){
+            int time = Integer.parseInt(sc.next().replace(":", ""));
+            String name = sc.next();
+            
+            ArrayList<String> tmp = new ArrayList<>();
+            if(students.containsKey(time)){
+                tmp = students.get(time);
+                tmp.add(name);
+
+                students.put(time, tmp);
+            }
+            else{
+                tmp.add(name);
+                students.put(time, tmp);
+            }
+        }
+
+        int answer = 0;
+
+        Set<String> first = new HashSet<>();
+        Set<String> second = new HashSet<>();
+
+        // 08:00 11:22 18:33
+        for (String key : students.keySet()) {
+            int current = Integer.parseInt(key.replace(":", ""));
+            
+            // 조건 1
+            if(current <= s){
+                first.addAll(students.get(key));
+            }
+            
+            // 조건 2
+            if(e <= current && current <= p){
+                second.addAll(students.get(key));
+            }
+
+        }        
+
+        System.out.println(first);
+        System.out.println(second);
+        first.retainAll(second);
+
+        // System.out.println(first);
+        System.out.println(first.size());
+        
+        
+    }
+}
