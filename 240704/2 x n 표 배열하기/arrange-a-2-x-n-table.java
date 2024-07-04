ba8507a157e876bf22c80d7f068a21faec2927a7
HEEJEONG OH <62127798+HeeJeongOh@users.noreply.github.com>
2024-07-04 15:00:58 +0900

    Title: 2 x n 표 배열하기, Time: 137ms, Memory: 13MB, Status: Passed - CodeTree

--- 240704/2 x n 표 배열하기/arrange-a-2-x-n-table.java
+++ 240704/2 x n 표 배열하기/arrange-a-2-x-n-table.java
@@ -24,22 +24,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[][] dp = new int[n+1][3];
+        long[][] dp = new long[n+1][3];
 
         dp[0][0] = 1;
         dp[0][1] = 1;
         dp[0][2] = 1;
 
-        for(int i = 1; i < n; i++){    
+        for(int i = 1; i <= n; i++){    
             // System.out.println(Arrays.toString(dp[i-1]));
-            dp[i][0] = dp[i-1][0] + dp[i-1][1] + dp[i-1][2];
-            dp[i][1] = dp[i-1][0] + dp[i-1][2];
-            dp[i][2] = dp[i-1][0] + dp[i-1][1];
+            dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % 10007;
+            dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % 10007;
+            dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % 10007;
         }
 
         // System.out.println(Arrays.toString(dp[n]));
 
-        long answer = dp[n-1][0] + dp[n-1][1] + dp[n-1][2];
-        System.out.println(answer % 10007);
+        long answer = dp[n][0];
+        System.out.println(answer);
     }
 }
