e5a9e3b62c86191bf939b4d29187f8a4f86038c1
HEEJEONG OH <62127798+HeeJeongOh@users.noreply.github.com>
2024-07-04 14:36:14 +0900

    Title: 2 x n 표 배열하기, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240704/2 x n 표 배열하기/arrange-a-2-x-n-table.java
+++ 240704/2 x n 표 배열하기/arrange-a-2-x-n-table.java
@@ -3,28 +3,14 @@ import java.util.*;
 - 2*n 격자 위 각 칸에 물건을 놓되 인접한 두 칸에 모두 물건이 올려져있으면 안된다.
 - 아무것도 배치하는 것도 하나의 경우의 수
 
-n = 3 => 17 = 1 + 6 + 8 + 2
-k = 0
-    0 0 0
-    0 0 0
-k = 1
-    1 0 0   0 1 0   0 0 1   0 0 0   0 0 0   0 0 0 
-    0 0 0   0 0 0   0 0 0   1 0 0   0 1 0   0 0 1
-k = 2
-    1 0 1   1 0 0   1 0 0   0 1 0   0 1 0   0 0 1   0 0 1   0 0 0
-    0 0 0   0 1 0   0 0 1   1 0 0   0 0 1   1 0 0   0 1 0   1 0 1
-k = 3
-    1 0 1   0 1 0
-    0 1 0   1 0 1
-
-n = 4 - 1 + 8 + 30(10 + 20) + 2
-k = 0 : 1
-k = 1 : 8
-k = 2 : 9
-    1 0 0 0
-    0 0 0 0
-k = 3 : 
-k = 4 : 2
+n = 1 : 3 = 1 + 2
+n = 2 : 7 = 1 + 4 + 2
+n = 3 : 17 = 1 + 6 + 8 + 2
+    k = 2 : 8 = 6 + 2
+    1 0 0   1 0 0   0 1 0  0 1 0   0 0 1   0 0 1   1 0 1   0 0 0
+    0 1 0   0 0 1   1 0 0  0 0 1   0 1 0   1 0 0   0 0 0   1 0 1
+n = 4 : 41 = 1 + 8 + (00) + (00) + 2
+n = 5 : 99 = 1 + 10 + (00) + (00) + (00) + 2;
 */
 public class Main {
     public static void main(String[] args) {
@@ -36,21 +22,16 @@ public class Main {
 
         dp[0] = 1;
         dp[1] = 2*n;
-
-        int answer = dp[0] + dp[1];
-
-
         for(int k = 2; k < n; k++){
-            int sum = 0;
-            for(int i = 0; i < k; i++){
-                sum += dp[i];
-            }
-            dp[k] = sum+1;
-            answer += dp[k];
+            dp[k] = (dp[k-1] - k) * 2;
         }
-        answer += 2;
+        dp[n] = 2;
         // System.out.println(Arrays.toString(dp));
-        System.out.println(answer % 10007);
 
+        int answer = 0;
+        for(int i = 0; i <= n; i++){
+            answer += dp[i];
+        }
+        System.out.println(answer);
     }
 }
