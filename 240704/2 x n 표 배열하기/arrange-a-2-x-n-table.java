3181885db705854c00d2dcac7c23a058b5b9b374
HEEJEONG OH <62127798+HeeJeongOh@users.noreply.github.com>
2024-07-04 14:48:49 +0900

    Title: 2 x n 표 배열하기, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240704/2 x n 표 배열하기/arrange-a-2-x-n-table.java
+++ 240704/2 x n 표 배열하기/arrange-a-2-x-n-table.java
@@ -3,14 +3,20 @@ import java.util.*;
 - 2*n 격자 위 각 칸에 물건을 놓되 인접한 두 칸에 모두 물건이 올려져있으면 안된다.
 - 아무것도 배치하는 것도 하나의 경우의 수
 
-n = 1 : 3 = 1 + 2
-n = 2 : 7 = 1 + 4 + 2
-n = 3 : 17 = 1 + 6 + 8 + 2
-    k = 2 : 8 = 6 + 2
-    1 0 0   1 0 0   0 1 0  0 1 0   0 0 1   0 0 1   1 0 1   0 0 0
-    0 1 0   0 0 1   1 0 0  0 0 1   0 1 0   1 0 0   0 0 0   1 0 1
-n = 4 : 41 = 1 + 8 + (00) + (00) + 2
-n = 5 : 99 = 1 + 10 + (00) + (00) + (00) + 2;
+[시도1]
+- 무지성 점화식 시도
+
+[해설]
+- 경우의 수 나누기
+    dp[i][0]: i 번째 열에 아무것도 배치하지 않은 경우의 수
+    dp[i][1]: i 번째 열의 첫 번째 행에만 물건을 배치한 경우의 수
+    dp[i][2]: i 번째 열의 두 번째 행에만 물건을 배치한 경우의 수
+
+- 점화식 만들기
+    dp[i][0] = dp[i-1][0] + dp[i-1][1] + dp[i-1][2]
+    dp[i][1] = dp[i-1][0] + dp[i-1][2]
+    dp[i][2] = dp[i-1][0] + dp[i-1][1]
+
 */
 public class Main {
     public static void main(String[] args) {
@@ -18,20 +24,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[] dp = new int[n+1];
+        int[][] dp = new int[n+1][3];
 
-        dp[0] = 1;
-        dp[1] = 2*n;
-        for(int k = 2; k < n; k++){
-            dp[k] = (dp[k-1] - k) * 2;
-        }
-        dp[n] = 2;
-        // System.out.println(Arrays.toString(dp));
+        dp[0][0] = 1;
+        dp[0][1] = 1;
+        dp[0][2] = 1;
 
-        int answer = 0;
-        for(int i = 0; i <= n; i++){
-            answer += dp[i];
+        for(int i = 1; i <= n; i++){    
+            // System.out.println(Arrays.toString(dp[i-1]));
+            dp[i][0] = dp[i-1][0] + dp[i-1][1] + dp[i-1][2];
+            dp[i][1] = dp[i-1][0] + dp[i-1][2];
+            dp[i][2] = dp[i-1][0] + dp[i-1][1];
         }
-        System.out.println(answer);
+
+        // System.out.println(Arrays.toString(dp[n]));
+
+        long answer = dp[n][0] + dp[n][1] + dp[n][2];
+        System.out.println(answer % 10007);
     }
 }
