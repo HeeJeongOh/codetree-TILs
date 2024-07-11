e1fe67a1dfdcda32805d63bcba0536104fc77149
HEEJEONG OH <62127798+HeeJeongOh@users.noreply.github.com>
2024-07-11 18:37:04 +0900

    Title: 블럭쌓는 명령, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240711/블럭쌓는 명령/block-stacking-commands.java
+++ 240711/블럭쌓는 명령/block-stacking-commands.java
@@ -3,7 +3,11 @@ import java.util.*;
 - n개의 칸, ai부터 bi까지 각각 블럭을 1번 쌓기
 - 블럭의 개수 오름차순 정렬
 
+[시도1] 이중포문
+[시도2] 검색 - 카운트 정렬
+1. 값의 개수를 세어 다시 넣기
 */
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,7 +18,6 @@ public class Main {
         int k = sc.nextInt();
 
         int[] blocks = new int[n+1];
-
         for(int i = 0; i < k; i++){
             int a = sc.nextInt();
             int b = sc.nextInt();
@@ -22,9 +25,25 @@ public class Main {
                 blocks[j] += 1;
             }    
         }
-    
-        Arrays.sort(blocks);
-        System.out.println(blocks[(n+1)/2]);
+        // Arrays.sort(blocks);
+        // System.out.println(Arrays.toString(blocks));
 
+        // Count Sorting
+        int[] cnt = new int[n+1];
+        for(int i = 1; i <= n; i++){
+            cnt[blocks[i]] += 1;
+        }
+        // System.out.println(Arrays.toString(cnt));
+        
+        int idx = 0;
+        int[] answer = new int[n+1];
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j < cnt[i]; j++){
+                answer[idx] = i;
+                idx += 1;
+            }
+        }
+        // System.out.println(Arrays.toString(answer));
+        System.out.println(answer[n/2]);
     }
 }
