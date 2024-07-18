c4d67ffbbeaf56791793983932369aba8a5f186e
HEEJEONG OH <62127798+HeeJeongOh@users.noreply.github.com>
2024-07-18 13:59:13 +0900

    Title: 십진수와 이진수, Time: 111ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240718/십진수와 이진수/decimal-and-binary-number.java
+++ 240718/십진수와 이진수/decimal-and-binary-number.java
@@ -1,18 +1,28 @@
 import java.util.*;
+import java.math.BigInteger;
+/*
+input : 10001111110001000101000001000100111100110101100011000011011001111000100110110000110101110101100001001100010111000101000100010010011000000010010
+
+Big Integer : lon
+*/
 
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        String binaryNumber = sc.next();
+        String binaryStr = sc.next();
 
-        // n2 -> n10 -> *17
-        int decimalNumber = Integer.parseInt(binaryNumber, 2) * 17;
-        // n10 -> n2
-        binaryNumber = Integer.toBinaryString(decimalNumber);
-        
-        System.out.println(binaryNumber);
+        // binary to decmial
+        BigInteger decimalValue = new BigInteger(binaryStr, 2);
+        BigInteger mulValue = new BigInteger("17");
+
+        decimalValue.multiply(mulValue);
+        // System.out.println("Decimal value: " + decimalValue);
+
+        // // to binary
+        String answer = decimalValue.toString(2);
+        System.out.println(answer);
 
     }
 }
