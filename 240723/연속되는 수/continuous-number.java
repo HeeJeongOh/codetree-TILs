95b9826748f5a9638cce37050c3f58d9ad2d28c5
HEEJEONG OH <62127798+HeeJeongOh@users.noreply.github.com>
2024-07-23 18:46:28 +0900

    Title: 연속되는 수, Time: 143ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240723/연속되는 수/continuous-number.java
@@ -0,0 +1,54 @@
+import java.util.*;
+/*
+- 하나의 숫자를 선택하여 그 숫자를 전부 제외
+- 연속하여 동일한 숫자가 나오는 횟수가 최대가 되도록
+
+2 / 7 / 3 / 7 7 / 3 / 7 / 5 / 7
+2 / 2 / 2
+
+1. k 후보 구하기
+2. k 숫자 제거해가며 연속 숫자 세기
+
+1. 연속된 경우 미리 구분짓기
+2. 
+
+*/
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] numbers = new int[N];
+        Set<Integer> numset = new HashSet<>();
+        for(int i = 0; i < N; i++){
+            numbers[i] = sc.nextInt();
+            numset.add(numbers[i]);
+        }
+        // System.out.println(Arrays.toString(numbers));
+
+        int answer = 0;
+        for(int k : numset){
+            int cnt = 1;
+            int prenum = numbers[0];
+
+            for(int i = 1; i < N; i++){
+                if(numbers[i] == k){
+                    continue;
+                }
+                if(prenum != numbers[i]){
+                    if(answer < cnt) {
+                        answer = cnt; 
+                    }
+                    prenum = numbers[i];
+                    cnt = 1;                    
+                }
+                else{
+                    cnt += 1;
+                }   
+            }
+            // System.out.println("k : " + k + " / cnt : " + answer);
+        }            
+        System.out.println(answer);
+    }
+}
